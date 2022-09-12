65856a10a094464cbab5325c74a80f17308355fd
cofeedcognac <okesmimi@gmail.com>
2022-09-12 16:47:30 -0700

    adv

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, e;
+
+	i = 48;
+	e = 48;
+
+	while (e < 58)
+	{
+		i = 48;
+		while (i < 58)
+		{
+			putchar(e);
+			putchar(i);
+			if (i == 57 && e == 57)
+			{
+				break;
+			}
+
+			
